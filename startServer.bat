745bcbc35b4ef11d2e6d7fe0c579d00fe7bed26a
Charles Noble <Charles.Noble@dmv.ca.gov>
2022-07-29 14:11:59 -0700

    Remove old Ant build

--- startServer.bat
+++
@@ -1,2 +0,0 @@
-REM java -classpath "C:\Chris\Java\Projects\lpd-0.5.4\lib\log4j-1.2.8.jar;C:\Chris\Java\Projects\lpd-0.5.4\dist\lib\lpd-20031206.jar" org.simoes.lpd.Main
-java -classpath ".\lib\log4j-1.2.8.jar;.\dist\lib\lpd-20031206.jar" org.simoes.lpd.Main
